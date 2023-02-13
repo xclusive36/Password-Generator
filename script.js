4a7b435c5f47ddc806eaed85713a0b18bc71b810
Joshua Cavell <xclusive36@gmail.com>
2023-02-13 09:58:06 -0500

    Added variables to script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // Assignment code here
-
+const minimumPasswordLength = 8;
+const maximumPasswordLength = 128;
+let password = "";
+let passwordCharacters = "";
+const passwordLowercase = "abcdefghijklmnopqrstuvwxyz";
+const passwordUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const passwordNumbers = "0123456789";
+const passwordSpecialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
